3e3441ca224aef1a24cb75e7149780c3ea5674ea
yabsiraa <88306942+yabsiraa@users.noreply.github.com>
2021-10-05 19:52:06 +0300

    Update and rename _pritnt.c to _pritntf.c

---
+++ _pritntf.c
@@ -0,0 +1,46 @@
+
+#include <stdarg.h>
+#include "main.h"
+#include <stdlib.h>
+
+int _printf(const char *format, ...)
+{
+    va_list ap;
+    const char *p;
+    char *cval;
+    char *sval;
+    int ival;
+    double dval;
+
+    va_start(ap, format);
+
+    for(p = format; *p; p++)
+    {
+        if (*p != '%')
+        {
+           _putchar (*p);
+            continue;
+        }
+        switch (*++p)
+        {
+	case 'c':
+		cval = va_arg(ap, char *);
+		_putchar(*cval);
+		break;
+            case 'd':
+            ival = va_arg(ap, int);
+            _putint(ival );
+            break;
+            case 'f':
+            dval = va_arg(ap, double);
+            _putint(dval);
+            break;
+            case 's':
+            for (sval = va_arg(ap, char *); *sval; sval++)
+                _putchar(*sval);
+            break;
+        }
+    }
+    va_end(ap);
+    return 0;
+}
